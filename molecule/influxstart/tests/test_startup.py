b826befc86cc9105e35c61947b1121a02a198cfd
Travis Holton <heytrav@protonmail.com>
2019-12-16 06:31:53 +1300

    fixed linting and added a test for listening port

--- molecule/influxstart/tests/test_startup.py
+++ molecule/influxstart/tests/test_startup.py
@@ -19,3 +19,9 @@ def test_influxdb_running(host):
     """
     service = host.service('influxdb')
     assert service.is_running
+
+
+def test_influxdb_listening_on_port(host):
+    """Test that influxdb is listening on port
+    """
+    assert host.socket("tcp://0.0.0.0:8086").is_listening
